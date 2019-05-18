2788abe94e82b767257d8cba7cdf0b3cf48ab5ec
StefanEvanghelides <stefan.evanghelides@gmail.com>
2019-05-18 12:49:26 +0200

    update on preprocessing

--- Preprocess/preprocess.py
+++ Preprocess/preprocess.py
@@ -1,15 +1,19 @@
 import sys
 import os
 
+import cv2
+
 import matplotlib
 import matplotlib.pyplot as plt
+
+import numpy as np
 
 from skimage import img_as_float, img_as_ubyte
 from skimage.exposure import adjust_gamma, adjust_log
 from skimage.io import imread, imsave, imshow
-from skimage.filters import threshold_sauvola
+from skimage.filters import threshold_sauvola, threshold_otsu
 
-from tools.peakdetect import *
+from Preprocess.tools.peakdetect import *
 
 matplotlib.rcParams['font.size'] = 9
 
@@ -21,86 +25,118 @@ def readImage(filename):
 
 
 def constrast(image, method):
-
     if (method == 1): contrastedImage = adjust_gamma(image) 
     if (method == 2): contrastedImage = adjust_log(image)
 
     return contrastedImage
 
 
-def binarise (image):
+def binariseSauvola(image):
     window_size = 25
-    thresh_sauvola = threshold_sauvola(image, window_size=window_size)
+    k = 0.5 # This is optional parameter
+    r = 128 # This is optional parameter
+    thresh_sauvola = threshold_sauvola(image, window_size=window_size, k=k, r=r)
 
-    binary_sauvola = image > thresh_sauvola
+    binarised_sauvola = image > thresh_sauvola
+    binarised_sauvola = img_as_ubyte(binarised_sauvola)
 
-    return binary_sauvola
+    return binarised_sauvola
 
 
-def writeBinarisedImage(image, filename):
-
-    path_output = os.path.abspath(filename)
+def binariseOtsu(image):
+    binarised_otsu = image > threshold_otsu(image)
+    binarised_otsu = img_as_ubyte(binarised_otsu)
     
+    return binarised_otsu
+
+
+def writeImage(image, filename):
+    path_output = os.path.abspath(filename)
     imsave(fname=path_output,arr=image)
 
-
-
-
-
-def segment(image):
-    max_peaks, min_peaks = peakdetect(image, lookahead=40)
-    print(min_peaks)
-    return ''
-
-
-#    peak = []
-#    for y in peaks[0]:
-##        peak.append(y[0])
-  #      # plt.plot(y[0], y[1], "r*")
-   #     cv2.line(rotated2, (0, y[0]), (W, y[0]), (255, 0, 0), 3)
 
 
 
 ####### Main ########
 #####################
 
-inputImageName = sys.argv[1]
-outputImageName = sys.argv[2]
+def preprocess(inputImageName, outputDirectory):
+    # Read name
+    inputImage = readImage(inputImageName)
+
+    # Binarise using Sauvola binarisation with threshold=0.5
+    binarisedSauvola = binariseSauvola(inputImage)
+    binarisedOtsu = binariseOtsu(inputImage)
+
+    # Save binarised image
+    writeImage(binarisedSauvola, outputDirectory+"/binarisedSauvola.jpg")
+    writeImage(binarisedOtsu, outputDirectory+"/binarisedOtsu.jpg")
 
 
-# Read name
-inputImage = readImage(inputImageName)
+    # Get the connected componenets
+    # Credit: https://stackoverflow.com/questions/46441893/connected-component-labeling-in-python
+    ret, labels = cv2.connectedComponents(binarisedOtsu)
 
-# Binarise using Sauvola binarisation with threshold=0.5
-binarisedImage = binarise (inputImage)
-binarisedImage = img_as_ubyte(binarisedImage)
+    # Map component labels to hue val
+    label_hue = np.uint8(179 * labels / np.max(labels))
+    blank_ch = 255 * np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
 
-# Save binarised image
-writeBinarisedImage(binarisedImage, outputImageName)
+    # cvt to BGR for display
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
-##########################
+    # set bg label to black
+    labeled_img[label_hue == 0] = 0
 
-### Segment it ###
-
-segmentedImage = segment(binarisedImage)
-
-
-
+    writeImage(labeled_img, outputDirectory+"/labeled_otsu.jpg")
 
 
-# Show all preprocessing steps
+    # Second attempt:
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(binarisedOtsu, connectivity=4)
+    print (nb_components)
+    print (output)
+    print (stats)
+    print (centroids)
 
-plt.figure(figsize=(8, 3))
+    # Get the largest component
+    sizes = stats[:, -1]
+    max_label = 1
+    max_size = sizes[1]
+    for i in range(2, nb_components):
+        if sizes[i] > max_size:
+            max_label = i
+            max_size = sizes[i]
+    labeled_img_with_stats = np.zeros(output.shape)
+    labeled_img_with_stats[output == max_label] = 255
 
-plt.subplot(1, 2, 1)
-plt.imshow(inputImage, cmap=plt.cm.gray)
-plt.title('Original')
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(binarisedImage, cmap=plt.cm.gray)
-plt.title('Sauvola Threshold')
-plt.axis('off')
+    writeImage(labeled_img_with_stats, outputDirectory+"/labeled_otsu_with_stats.jpg")
 
 
-plt.show()
+
+    print ("Preprocessing successful!")
+
+    ##########################
+
+
+
+    # Show all preprocessing steps
+
+    # plt.figure(figsize=(8, 3))
+
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(inputImage, cmap=plt.cm.gray)
+    # plt.title('Original')
+    # plt.axis('off')
+
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(binarisedImage, cmap=plt.cm.gray)
+    # plt.title('Sauvola Threshold')
+    # plt.axis('off')
+
+
+    # plt.show()
+
+if __name__ == "__main__":
+    inputImageName = sys.argv[1]
+    outputDirectory = sys.argv[2]
+    preprocess(inputImageName, outputDirectory)
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from Preprocess.preprocess import *
+
+
+inputImage = "Input/P168-Fg016-R-C01-R01-fused.jpg"
+outputDirectory = "Output"
+
+# First, preprocess images.
+preprocess(inputImage,outputDirectory)
