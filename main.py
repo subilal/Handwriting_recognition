485920b5e4456588edb2576b92f70d40972b857d
StefanEvanghelides <stefan.evanghelides@gmail.com>
2019-06-10 00:04:44 +0200

    added arguments to the program. Split the input folder into 3.

--- main.py
+++ main.py
@@ -6,21 +6,56 @@ import glob
 from Preprocess.preprocess import *
 from Segment.segment import *
 
+#################
+### Arguments ###
+#################
 
+# Parse arguments
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-d", "--debug", help="run program in debug mode",
+                                    action="store_true")
+group.add_argument("-f", "--fast",  help="skip over intermediary results (used to speed-up the program)",
+                                    action="store_true")
+parser.add_argument("-e", "--extension", type=str, default="jpg",
+                                    help="specify the extension (default=.jpg)",)
+parser.add_argument("input_dir",    help="Specify the input directory")
+args = parser.parse_args()
 
-output_directory = "Output"
+## Process parsed arguments
+#if args.debug:
+#    runmode = 2  # Show debug messages and intermediary steps
+#elif args.fast:
+#    runmode = 0  # Do not show any debug messages or intermediary steps
+#else:
+#    runmode = 1  # Default behaviour: show intermediary steps, but no debug
+debug = args.debug
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('--d', help='run program in debug mode')
-debug = False
+print ("extension=",args.extension)
 
-#print (input_directory_argument)
-#input_directory = glob.glob(input_directory_argument)
+#print ("runmode=" + str(runmode)) 
 
+# Set I/O directory names
+input_directory = os.path.abspath(args.input_dir)
+extension = "*" + args.extension
+files_directory = os.path.join(input_directory, extension)
+files = glob.glob(files_directory)
 
+output_directory = "Output" # This is a hardcoded value, but can easily be integrated in the command line args.
+remove_directory(output_directory)
+ensure_directory(output_directory)
 
-input_directory = "Input"
-files = glob.glob("Input/*fused.jpg")
+################
+##### Main #####
+################
+
+# Run the program
+print ("")
+print ("Processing files from the folder:")
+print ("    " + input_directory)
+print ("with extension:")
+print ("    " + extension)
+print ("")
 for image_name in files:
     _, image = os.path.split(image_name)
     
